155bc576d3b94d1faa90a8c9f203e534cbd6dc7e
Mai Mohamed <maim51084@gmail.com>
2024-07-27 14:01:54 +0300

    Structure Commit

--- app/src/main/java/com/example/recipeapp/view/auth/AuthActivity.kt
+++ app/src/main/java/com/example/recipeapp/view/auth/AuthActivity.kt
@@ -1,16 +1,17 @@
-package com.example.recipeapp
+package com.example.recipeapp.view.auth
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.recipeapp.R
 
-class RecipeActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_recipe)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
