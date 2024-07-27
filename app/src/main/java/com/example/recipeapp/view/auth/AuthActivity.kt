4f36ec9219dd27d220d02e17ac2bddce153b2a88
Ahmed-Abdelghany-11 <anofal786@gmail.com>
2024-07-27 20:24:36 +0300

    add UI Splash Screen

--- app/src/main/java/com/example/recipeapp/view/auth/AuthActivity.kt
+++ app/src/main/java/com/example/recipeapp/view/auth/AuthActivity.kt
@@ -10,7 +10,7 @@ import com.example.recipeapp.R
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
