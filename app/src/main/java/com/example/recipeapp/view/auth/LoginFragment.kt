27b4ab796c8c99b94034c6c08a192fa9f1f903ff
Alaa551 <alaahassan2019th@gmail.com>
2024-07-28 20:30:14 +0300

    edit navigation commit

--- app/src/main/java/com/example/recipeapp/view/auth/LoginFragment.kt
+++ app/src/main/java/com/example/recipeapp/view/auth/LoginFragment.kt
@@ -1,9 +1,22 @@
 package com.example.recipeapp.view.auth
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.recipeapp.R
+import com.google.android.material.button.MaterialButton
 
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val signInBtn= view.findViewById<MaterialButton>(R.id.signin_button)
+        signInBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_recipeActivity)
+        }
+
+    }
 }
--- app/src/main/java/com/example/recipeapp/view/recipe/RecipeActivity.kt
+++ app/src/main/java/com/example/recipeapp/view/recipe/RecipeActivity.kt
@@ -3,11 +3,22 @@ package com.example.recipeapp.view.recipe
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.recipeapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class RecipeActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +28,19 @@ class RecipeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+      val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+         navController = navHostFragment.navController
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        NavigationUI.setupWithNavController(bottomNav,navController)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolBar)
+        setSupportActionBar(toolbar)
+        NavigationUI.setupWithNavController(toolbar,navController)
+
+//        val homeFabButton= findViewById<FloatingActionButton>(R.id.floating)
+
+
+
     }
 }
