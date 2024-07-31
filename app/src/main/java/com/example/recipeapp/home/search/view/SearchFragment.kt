39fff43fb5b88d16847afbdaf25390b369c5503f
Alaa551 <alaahassan2019th@gmail.com>
2024-07-31 12:41:22 +0300

    search ui

---
+++ app/src/main/java/com/example/recipeapp/home/search/view/SearchFragment.kt
@@ -0,0 +1,13 @@
+package com.example.recipeapp.home.search.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.recipeapp.R
+
+
+class SearchFragment : Fragment(R.layout.fragment_search) {
+
+}
