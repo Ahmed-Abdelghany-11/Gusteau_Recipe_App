17f836a19fc1e72aebfac78198669f80c69a9541
ZOZOGIT1 <zizo.tarek.ghonim@gmail.com>
2024-08-03 16:37:30 +0300

    show toast to indicate no internet available

--- app/src/main/java/com/example/recipeapp/authentication/AuthActivity.kt
+++ app/src/main/java/com/example/recipeapp/authentication/AuthActivity.kt
@@ -1,10 +1,22 @@
 package com.example.recipeapp.authentication
 
+import android.app.Dialog
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkInfo
+import android.net.NetworkRequest
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.getSystemService
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
@@ -13,8 +25,10 @@ import androidx.navigation.NavOptions
 import androidx.navigation.findNavController
 import com.example.recipeapp.R
 import com.example.recipeapp.data.SharedPreference.AuthSharedPref
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.security.AccessController.getContext
 
 class AuthActivity : AppCompatActivity() {
     private lateinit var navController: NavController
@@ -31,6 +45,18 @@ class AuthActivity : AppCompatActivity() {
         navController = findNavController(R.id.nav_host_fragment)
 
         handleDeepLink(intent)
+
+        val connectivityManager = getSystemService(ConnectivityManager::class.java)
+        //val currentNetwork = connectivityManager.activeNetwork
+        connectivityManager.registerDefaultNetworkCallback(object: ConnectivityManager.NetworkCallback() {
+            override fun onLost(network: Network) {
+                Toast.makeText(applicationContext, "Internet is unavailable", Toast.LENGTH_LONG).show()
+//                showDialog()
+//                navController.navigate(R.id.action_global_to_noInternetFragment)
+            }
+
+        })
+
 
     }
 
@@ -53,5 +79,23 @@ class AuthActivity : AppCompatActivity() {
         super.onNewIntent(intent)
         handleDeepLink(intent)
     }
+
+    private fun showDialog() {
+        runOnUiThread {
+            if (!isFinishing && !isDestroyed) {
+                MaterialAlertDialogBuilder(this)
+                    .setTitle("Remove Meal From Favorites")
+                    .setMessage("Are you sure you want to remove this meal from favorites?")
+                    .setPositiveButton("Remove") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .setNegativeButton("Cancel") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .show()
+            }
+        }
+    }
+
 
 }
